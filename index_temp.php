b794541fd893c5fa81bac197644da28bfd8ca407
Greg Alexander <gregory.l.alexander@gmail.com>
2015-08-16 12:52:14 -0500

    Temp index page.

---
+++ index_temp.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="description" content="Brodie Park Crossfit" />
+    <meta name="keywords" content="Brodie Park, Brodie Park Crossfit, CrossFit Kids austin, austin, ATX, texas, TX, Crossfit Austin, Crossfit TX, South Austin Crossfit, Crossfit, Crossfit Kids, crossfight" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Brodie Park Crossfit | Austin, TX</title>
+    <link href='http://fonts.googleapis.com/css?family=Graduate' rel='stylesheet' type='text/css' />
+    <link href='http://fonts.googleapis.com/css?family=Economica:400,700' rel='stylesheet' type='text/css' />
+    <link href='http://fonts.googleapis.com/css?family=Permanent+Marker' rel='stylesheet' type='text/css' />
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js" type="text/javascript"></script>
+
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link rel="icon" type="image/x-icon" href="http://www.brodieparkcrossfit.com/favicon.ico">
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body style="position: relative; width: 100%; height: 100%;">
+    <div style="position: relative; top: 0; bottom: 0; left: 0; right: 0; background-color: #000000">
+        <img style="margin-top: 100px" src="images/BPC_Logo_Black.jpg" alt="background">
+    </div>
+    <div style="position: relative; margin-top: 20px; font-family: 'Economica', sans-serif; font-size: 25px; color: #ffffff; font-weight: 600;">
+        We've moved. Check out <a style="text-decoration: none; color: #ffffff" href="http://www.brodiepark.com">BrodiePark.com</a>.
+    </div>
+</body>
+</html>
