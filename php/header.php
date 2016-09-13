1fdafb242d46d2393b1c73e94c27560fa1819de2
Greg Alexander <gregory.l.alexander@gmail.com>
2016-09-12 22:38:57 -0500

    yelp icon and link

--- php/header.php
+++ php/header.php
@@ -1,13 +1,14 @@
 <header>
-	<a class="bpc-logo-container">
+    <a class="bpc-logo-container">
         <img src="images/BPC_Logo_Black.jpg" id="logo" />
     </a>
-	<ul class="rightnav">
-		<li><span title="google plus"><a href="https://plus.google.com/+BrodieParkCrossFitAustin" target="_blank"><img class="header_img" src="images/Google_plus.png" alt="plus"></a></span></li>
-		<li><span title="facebook"><a href="https://www.facebook.com/BrodieParkCrossFit" target="_blank"><img class="header_img" src="images/Facebook.png" alt="fb"></a></span></li>
-		<li><span title="twitter"><a href="https://twitter.com/brodieparkxfit" target="_blank"><img class="header_img" src="images/Twitter2.png" alt="tweet"></a></span></li>
+    <ul class="rightnav">
+        <li><span title="google plus"><a href="https://plus.google.com/+BrodieParkCrossFitAustin" target="_blank"><img class="header_img" src="images/Google_plus.png" alt="plus"></a></span></li>
+        <li><span title="facebook"><a href="https://www.facebook.com/BrodieParkCrossFit" target="_blank"><img class="header_img" src="images/Facebook.png" alt="fb"></a></span></li>
+        <li><span title="twitter"><a href="https://twitter.com/brodieparkxfit" target="_blank"><img class="header_img" src="images/Twitter2.png" alt="tweet"></a></span></li>
         <li><span title="instagram"><a href="https://instagram.com/brodieparkcrossfit/" target="_blank"><img class="header_img" src="images/Instagram.png" alt="insta"></a></span></li>
-	</ul>
+        <li><span title="yelp"><a href="https://www.yelp.com/biz/brodie-park-crossfit-austin" target="_blank"><img class="header_img yelp-icon" src="images/yelp.png" alt="yelp"></a></span></li>
+    </ul>
 
-	<div class="clear"></div>
+    <div class="clear"></div>
 </header>
